089cf69d0f709ceffed48181cfe50f5af8498c53
Dhruv Makwana <dhruvubm@gmail.com>
2025-12-15 23:51:50 +0530

    Update output message locally in VS Code

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,6 +1,6 @@
 class HelloWorld:
     def main(self):
-        print("Hello, World! Edited directly on GitHub")
+        print("Hello, World! Edited locally in VS Code")
 
 
 if __name__ == "__main__":
